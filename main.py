4f74a8bba7bc07b003ac07039f0d80e43525b507
Xanthus <Xanthus58@protonmail.com>
2022-04-18 08:49:13 +0930

    0.1.3
    > Added a help command
    > Added a inventory command
    > Added invalid response error

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 from colorama import Fore, Back, Style
 from playsound import playsound
 from threading import Thread
@@ -30,7 +32,7 @@ def credits():
     enter()
     print("Email me on 'Xanthus58@protonmail.com'")
     enter()
-    print("Feel free to fork; submit issues; or otherwise interact with the project here!")
+    print("Feel free to fork; submit issues; and otherwise interact with the project here!")
     print("https://github.com/Xanthus58/CLI-Adventure-Game")
     enter()
     input("Press Enter to return to the main menu: ")
@@ -41,8 +43,11 @@ def scroll(str):
   for letter in str:
     sys.stdout.write(letter)
     sys.stdout.flush()
-    time.sleep(0.01)
+    time.sleep(0.03)
 
+cellactions = ["inv","Inv","Inventory","inventory","help","Help","Lamp","lamp","Look","look","Search","search","bed","Bed","Use","use","Drink", "drink","Bucket","bucket","Gate","gate","escape","Escape"]
+#-Helplist-
+Cellarhelplist = ["> Lamp\n","> Bed\n","> Bucket\n","> Gate\n",]
 #-Inventory-
 lamp = 0
 CellKey = 0
@@ -53,9 +58,33 @@ t5()
 scroll("It's dark, but you can see from the dimly lit lamp in the corner that you are in a dungeon.\n")
 t5()
 scroll("What do you do: ")
+response = input()
 
 while True:
-    response = input()
+
+    actions = ["inv","Inv","Inventory","inventory"]
+    for x in actions:
+        if x in response:
+            cls()
+            scroll("-Items-\n")
+            if lamp == 1:
+                scroll("> Lamp\n")
+            if CellKey == 1:
+                scroll("> Cell Key\n")
+            scroll("What do you do: ")
+            response = input()
+
+    actions = ["help","Help"]
+    for x in actions:
+        if response in x:
+            for x in Cellarhelplist:
+                cls()
+                scroll("Thease are the items/actions you can perform and interact with. In this room\n")
+                scroll(Cellarhelplist)
+                scroll("What do you do: ")
+                response = input()
+                break
+                
     actions = ["Lamp","lamp"]
     for x in actions:
         if lamp == 0:
@@ -68,7 +97,7 @@ while True:
                 lamp = 1
                 scroll("What do you do: ")
                 response = input()
-
+                
     actions = ["Look","look","Search","search"]
     for x in actions:
         if x in response:
@@ -82,7 +111,7 @@ while True:
                 scroll("You also see a dimly lit lamp in the corner of the cell\n")
             scroll("What do you do: ")
             response = input()
-
+            
     actions = ["bed","Bed"]
     for x in actions:
         if CellKey == 0:
@@ -94,6 +123,7 @@ while True:
                 CellKey = 1
                 scroll("What do you do: ")
                 response = input()
+         
         elif CellKey == 1:
             if x in response:
                 cls()
@@ -102,23 +132,26 @@ while True:
                 scroll("You slowly awaken; its still the same dark cell you went to sleep in. \n")
                 scroll("What do you do: ")
                 response = input()
-
+            
     actions = ["Use","use"]
     for x in actions:
         if x in response:
+            cls()
             scroll("You can not use this at this time.\n")
             scroll("What do you do: ")
             input()
+            
     actions = ["Drink", "drink","Bucket","bucket"]
     for x in actions:
         if x in response:
+            cls()
             scroll("You pick up the bucket, it smeels like rot.\n")
             scroll("You take a deep breath and gulp down the contents of the bucket\n")
             scroll("You collapse onto the floor, vomiting. As death firmly graps you.\n")
             scroll(Fore.RED+"You have perrished. Please try again"+Fore.WHITE)
             t()
             credits()
-            break
+            os._exit()
 
     actions = ["Gate","gate","escape","Escape"]
     for x in actions:
@@ -126,13 +159,23 @@ while True:
             if x in response:
                 cls()
                 scroll("You use the key to unlock the cell gate and step outside \n")
+                CellKey = 0
                 scroll(Fore.GREEN+"Succsess you beat the demo! Please tell me what you think! as this is only a proof of concept."+Fore.WHITE)
                 t()
                 credits()
-                break
+                os._exit()
+
         elif CellKey == 0:
             if x in response:
                 cls()
-                scroll("The gate is locked shut no amount of bashing; pulling; or punching will open it. There must be another way\n")
+                scroll("The gate is locked shut no amount of bashing; pulling; and punching will open it. There must be another way\n")
                 scroll("What do you do: ")
                 response = input()
+
+    if response != "inv" and response != "Inv" and response != "Inventory" and response != "inventory" and response != "help" and response != "Help" and response != "Lamp" and response != "lamp" and response != "Look" and response != "look" and response != "Search" and response != "search" and response != "bed" and response != "Bed" and response != "Use" and response != "use" and response != "Drink" and response !=  "drink" and response != "Bucket" and response != "bucket" and response != "Gate" and response != "gate" and response != "escape" and response != "Escape":
+        cls()
+        scroll(Fore.RED+ "Invalid Input; please enter again.\n"+Fore.WHITE)
+        scroll("What do you do: ")
+        response = input()
+        print(response) 
+   
